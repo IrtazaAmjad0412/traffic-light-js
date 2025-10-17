138833fca5f4d4be9e2b98c4375c0ac43076a993
IrtazaAmjad0412 <irtaza0412@gmail.com>
2025-10-17 01:12:50 -0400

    Finished functional traffic light section

--- src/FunctionalTrafficLight.jsx
+++ src/FunctionalTrafficLight.jsx
@@ -1,17 +1,43 @@
+import { useState } from "react";
+
+const trafficLightColors = ["red", "yellow", "green"];
+
 export const FunctionalTrafficLight = () => {
+  const [trafficLightIndex, setTrafficLightIndex] = useState(0);
+  const currentTrafficLightColor = trafficLightColors[trafficLightIndex];
+
+  const nextTrafficLight = () =>
+    trafficLightIndex === 0
+      ? setTrafficLightIndex(trafficLightColors.length - 1)
+      : setTrafficLightIndex(trafficLightIndex - 1);
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
         {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div
+          className={currentTrafficLightColor === "red" ? "circle red" : "circle black"}
+        ></div>
+        <div
+          className={
+            currentTrafficLightColor === "yellow" ? "circle yellow" : "circle black"
+          }
+        ></div>
+        <div
+          className={
+            currentTrafficLightColor === "green" ? "circle green" : "circle black"
+          }
+        ></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button
+        className="next-state-button"
+        onClick={() => {
+          nextTrafficLight();
+        }}
+      >
+        Next State
+      </button>
     </div>
   );
 };
-
-
-
